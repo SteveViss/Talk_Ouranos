2d3d10cf8205f93ccaf055b25a49bb2a8d527608
SteveViss <s.vissault@yahoo.fr>
2015-03-26 10:05:11 -0400

    publishing deck

---
+++ default.php
@@ -0,0 +1,251 @@
+﻿
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+    <head>
+        <title>Ouranos</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="author" content="www.ixmedia.com" />
+        <meta name="description" content="Consortium on Regional Climatology and Adaptation to Climate Change that brings together 250 scientists from different disciplines." />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+		<link rel="alternate" type="application/rss+xml" href="/en/rss/" title="Ouranos - fil RSS - Nouvelles" />
+		<link href="/css/global.css" rel="stylesheet" type="text/css" media="screen" />
+        <link href="/css/print.css" rel="stylesheet" type="text/css" media="print" />
+		<!--[if lt IE 7]>
+			<link href="/css/ie6.css" rel="stylesheet" type="text/css" media="screen" />
+			<script type="text/javascript" src="/js/unitpngfix.js"></script>
+		<![endif]-->
+		<script type="text/javascript" src="/js/jquery.js"></script>
+        <script type="text/javascript" src="/js/global.js"></script>
+        <script type="text/javascript" src="/js/smoothscroll.js"></script>        
+        
+        <script type="text/javascript">
+
+          var _gaq = _gaq || [];
+          _gaq.push(['_setAccount', 'UA-21024148-1']);
+          _gaq.push(['_setDomainName', 'ouranos.ca']);
+          _gaq.push(['_trackPageview']);
+        
+          (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+          })();
+        
+        </script><script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-6438794-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+
+	</head>
+
+	<style type="text/css">
+	#accueil #contenu #wrap #colonne-centre .section p {
+	font-size: 16px;
+}
+    </style>
+	<script type="text/javascript">
+function MM_preloadImages() { //v3.0
+  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+}
+    </script><script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-6438794-1', 'auto');
+  ga('send', 'pageview');
+function MM_swapImgRestore() { //v3.0
+  var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
+}
+function MM_findObj(n, d) { //v4.01
+  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
+    d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
+  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
+  for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
+  if(!x && d.getElementById) x=d.getElementById(n); return x;
+}
+
+function MM_swapImage() { //v3.0
+  var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
+   if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
+}
+    </script>
+
+	<body id="accueil" onLoad="MM_preloadImages('includes/img/synthesis2.jpg')">
+
+		<div id="page">
+
+			﻿<div id="entete" class="clearfix">
+	<div class="identite clearfix">
+		<div><h1><a href="/en/default.php" title="Ouranos - Homepage">Ouranos</a></h1></div>
+	  <p>Consortium on  Regional Climatology and Adaptation to Climate Change</p>
+</div>
+	<ul>
+		<li class="actif"><a href="/en/"><span>Home</span></a></li>
+              
+		<li><a href="/en/sitemap.php"><span>Site map</span></a></li>
+        
+                <li><a href="/en/media-room"><span>Media room</span></a></li>
+		<li><a href="/en/our-organisation/contact-us.php"><span>Contact us</span></a></li>
+        
+		        <li><a href="/fr/default.php" title="Version française"><span>Français</span></a></li>
+        	</ul>				<div class="superposition">
+					<h2>Openness</h2>
+				  <p style="margin-bottom: 0;">Encourage a climate of exchange to promote  multidisciplinary work within the organization and with the entire community.</p>
+		  </div>
+	</div>
+
+			<div id="contenu" class="clearfix">
+
+				<div id="wrap" class="clearfix">
+
+					<div id="colonne-gauche">
+						﻿<ul id="menu" class="clearfix">
+	<li>
+		<a href="/en/our-organisation/our-organisation.php">OUR ORGANIZATION</a>
+		<div>
+		  <ul class="clearfix">
+			  <li><a href="/en/our-organisation/background.php">Background</a></li>
+			  <li><a href="/en/our-organisation/mission-and-context.php">Mission and Context</a></li>
+			  <li><a href="/en/our-organisation/structure.php">Structure</a></li>
+              <li><a href="/en/our-organisation/about-ouranos.php">About Ouranos</a></li>
+			  			  			  <li><a href="/en/our-organisation/real-decoste-scholarship.php">Réal Décoste Scholarship</a></li>
+			</ul>
+		</div>
+	</li>
+	<li>
+		<a href="/en/scientific-program/scientific-program.php">SCIENTIFIC PROGRAM</a>
+		<div>
+			<ul class="clearfix">
+				<li>
+                    <a href="/en/scientific-program/climate-science/">Climate Science</a>
+                    <div>
+                        <ul class="sous-menu clearfix">
+                            <li><a href="/en/scientific-program/climate-science/climate-simulations/">Climate Simulation and Analysis</a></li>
+<!--                             <li><a href="/en/scientific-program/climate-sciences/hydroclimatic-analyses/">Hydroclimatic Analyses</a></li> -->
+                            <li class="dernier"><a href="/en/scientific-program/climate-science/climate-scenarios/">Climate Scenarios and Services</a></li>
+                        </ul>
+                    </div>
+                </li>
+				<li><a href="/en/scientific-program/impacts-adaptation/">Vulnerabilities, Impacts &amp; Adaptation</a>
+                    <div>
+                        <ul class="sous-menu clearfix">
+                            <li><a href="/en/scientific-program/impacts-adaptation/agriculture.php">Agriculture, Commercial Fisheries and Aquaculture</a></li>
+                            <li><a href="/en/scientific-program/impacts-adaptation/built-environment.php">Built Environment</a></li>
+                            <li><a href="/en/scientific-program/impacts-adaptation/ecosystem-biodiversity.php">Ecosystems &amp; biodiversity</a></li>
+                            <li><a href="/en/scientific-program/impacts-adaptation/energy-resources.php">Energy </a></li>
+                            <li><a href="/en/scientific-program/impacts-adaptation/forest-resources.php">Forest Resources</a></li>
+                            <li><a href="/en/scientific-program/impacts-adaptation/health.php">Health</a></li>
+                            <li><a href="/en/scientific-program/impacts-adaptation/maritime-environment.php">Maritime Environment</a></li>
+                            <li><a href="/en/scientific-program/impacts-adaptation/northern-environment.php">Northern Environment</a></li>
+							<li><a href="/en/scientific-program/impacts-adaptation/tourism.php">Tourism</a></li>
+							<li class="dernier"><a href="/en/scientific-program/impacts-adaptation/water-resources.php">Water Management</a></li>                      
+                        </ul>
+                    </div>
+                </li>
+			</ul>
+		</div>
+	</li>
+	<li>
+		<a href="/en/publications/scientific-publications.php">PUBLICATIONS</a>	
+		<div>
+			<ul class="clearfix">
+							
+						
+				<li class="dernier"><a href="/en/publications/list-of-published-articles.php">Published articles</a></li>
+				<li class="dernier"><a href="/en/publications/scientific-publications.php">Scientific publications</a></li>
+				
+			</ul>
+		</div>
+    </li>
+	<li><a href="../../fr/symposium/default.php">6e SYMPOSIUM</a></li>
+	<li><a href="../../fr/synthese2014/default.php">SYNTHESIS 2014</a></li>
+	<li><a href="/en/fact-sheets/">FACT SHEETS</a></li>
+  <li><a href="/en/links/"> LINKS</a></li>
+  <li><a href="/en/faq/">FAQ</a></li>
+</ul>
+					</div>
+
+				  <div id="colonne-centre">
+
+                    <div class="section">
+
+                   
+				  <h1 align="left">Processes</h1>
+
+                    	<p>Want to learn more about  our processes?<strong><a href="includes/processus.php" style="color: #4FBDE4"> </a><a href="../en/includes/processes.php" style="color: #58CFF3">Click here.</a></strong><a href="includes/processus.php"></a></p></div>
+                    <div></div>
+                    
+
+				    <h1 align="left">News</h1>
+
+			  <ul class="a-signaler">
+														 <li>
+								<h2><a href="news/fiche.php?id=151"><span>Job posting</span> <strong>Job Offer </strong></a></h2>
+								<p class="date">February 18, 2015</p>
+								<p>Ouranos is looking for a research assistant in the production
+and analysis of regional climate simulations. Details are in the job description.
+Deadline for application is March 12, 2015 at ressources.humaines@ouranos.ca.								<em><a href="news/fiche.php?id=151">[&nbsp;Details&nbsp;]</a></em></p>
+							</li>
+														 <li>
+								<h2><a href="news/fiche.php?id=149"><span>Press release</span> <strong>Vers l'adapatation - Synthèse 2014 (French only)</strong></a></h2>
+								<p class="date">December 5, 2014</p>
+								<p>Vers l'adaptation is the new synthesis of knowledge on climate change newly released by Ouranos.								<em><a href="news/fiche.php?id=149">[&nbsp;Details&nbsp;]</a></em></p>
+							</li>
+														 <li>
+								<h2><a href="news/fiche.php?id=148"><span>Publication</span> <strong>Ouranos' Strategic Plan (French only)</strong></a></h2>
+								<p class="date">December 5, 2014</p>
+								<p>Click here top access the Strategic Plan (French only).								<em><a href="news/fiche.php?id=148">[&nbsp;Details&nbsp;]</a></em></p>
+							</li>
+														 <li>
+								<h2><a href="news/fiche.php?id=147"><span>Publication</span> <strong>2013-2014 Annual report (French only)</strong></a></h2>
+								<p class="date">December 5, 2014</p>
+								<p>The 2013-2014 annual report is now available.								<em><a href="news/fiche.php?id=147">[&nbsp;Details&nbsp;]</a></em></p>
+							</li>
+												</ul>
+
+						<ul class="liste-fleches" style="margin-bottom: 0;">
+							<li><a href="/en/news/">See all news</a></li>
+					</ul>
+
+				  </div>
+				</div>
+
+
+			  <div class="pas-bordure" id="colonne-droite">
+				  <div class="pas-bordure">
+				    <p align="center"><a href="../fr/synthese2014/default.php" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('Image3','','includes/img/synthesis2.jpg',1)"><img src="includes/img/synthesis.jpg" alt="" width="175" height="132" id="Image3"></a></p>
+	             
+              
+              </div>
+                  
+			  <div class="pas-bordure">
+				    <p align="center"><a href="newsletter/default.php" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('Image2','','includes/img/newsletter2.jpg',1)"><img src="includes/img/newsletter.jpg" alt="" width="175" height="132" id="Image2"></a></p>
+	             
+              
+              </div><p align="center"><a href="http://adaptation.ouranos.ca" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('Image1','','includes/img/website2.jpg',1)"><img src="includes/img/website.jpg" alt="" width="175" height="132" id="Image1"></a></p>
+				  				  <div class="pas-bordure">
+			   <h2 align="left">Partner</h2>
+			   <img src="img/MEIE.jpg" alt="" width="200" height="85"/>
+			  </div>
+	</div>
+
+			<div id="pied">
+	<p id="ouranos">Ouranos</p>
+	<p>550 Sherbrooke West, West Tower, 19th floor, Montreal, Quebec, H3A&nbsp;1B9, Canada<br />Tel.: (514) 282-6464 Fax: (514) 282-7131<br />E-mail: <a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#119;&#101;&#98;&#109;&#101;&#115;&#116;&#114;&#101;&#64;&#111;&#117;&#114;&#97;&#110;&#111;&#115;&#46;&#99;&#97;">&#119;&#101;&#98;&#109;&#101;&#115;&#116;&#114;&#101;&#64;&#111;&#117;&#114;&#97;&#110;&#111;&#115;&#46;&#99;&#97;</a></p>
+</div>
+		</div>
+
+	</body>
+</html>
---
+++ index.md
@@ -0,0 +1,389 @@
+---
+title       : Ouranos et PostgreSQL.
+subtitle    : Vers un stockage spatialisé des GCMs à haute résolution?
+author      : Steve Vissault
+job         : Chaire de recherche du Canada en biogéographie et écologie des métacommunautés
+framework   : io2012        # {io2012, html5slides, shower, dzslides, ...}
+highlighter : highlight.js  # {highlight.js, prettify, highlight}
+license: by-nc-sa
+widgets: [bootstrap]
+ext_widgets : {rCharts: [libraries/nvd3, libraries/leaflet]}
+mode: selfcontained
+hitheme: solarized_light
+
+
+--- .segue .nobackground .dark
+
+## Mise en contexte...
+
+--- 
+
+## Contexte de stage 
+
+<div style='text-align: center;'>
+    <img src="./context1.png" alt="Context1" align="middle">
+</div>
+
+--- 
+
+## Contexte de stage 
+
+<div style='text-align: center;'>
+    <img src="./context2.png" alt="Context2" align="middle">
+</div>
+
+--- 
+
+## Contexte de stage
+
+<div style='text-align: center;'>
+    <img src="./context3.png" alt="Context3" align="middle">
+</div>
+
+--- 
+
+## Identification et réponse aux besoins 
+
+### **Objectif:**
+
+>Stocker les sorties climatiques d'Ouranos (Fichier Matlab/HDF5) dans une base de données spatialisée pour l'Est de l'Amérique du Nord.
+
+> -**Données mensuelles pour 3 variables climatiques**: 570 240 grilles
+
+> -**Données annuelles pour 19 biovariables climatiques**: 300 960 grilles
+
+### **Un stage en trois volets...**
+
+>0. Étude de faisabilité: Stockage et requêtes des grilles climatiques dans PostgreSQL (Automne 2014)
+>1. Conceptualiser et réaliser la base de données spatialisée dans PostgreSQL (Hiver 2015)
+>2. Créer un programme d'importation performant des fichiers HDFs (sorties climatiques d'Ouranos) vers la nouvelle base de données (Hiver 2015)
+
+--- 
+
+## La couverture spatiale...
+
+<div style='text-align: center'>
+    <img src="./cover_raster.png" alt="LogoPostgreSQL" align="middle">
+</div>
+
+--- .segue .nobackground .dark
+
+## Qu'est-ce que PostgreSQL?
+
+
+--- &twocol
+
+## Qu'est-ce que PostgreSQL? 
+
+
+*** =left
+
+Un Système de Gestion de Base de données (SGBD) gratuit et openSource depuis 1989, composé de:
+
+>- **Tables**: Comprend des enregistrements
+>- **Clés primaires**: Assure des enregistrement uniques
+>- **Jointures**: Permet de créer des relations entre les tables
+>- **Indexes**: Permet une recherche plus rapide
+>- **Intégrités référentielles:** Garantie l'intégrité des données 
+
+
+*** =right
+
+<div style='text-align: center'>
+    <img src="./postgres.jpg" height="300" alt="LogoPostgreSQL" align="middle">
+</div>
+
+--- 
+
+## Volet 1: Conceptualisation de la base de données
+
+Pour illustrer et comprendre, voici la base de données Ouranos:
+
+<div style='text-align: center'>
+    <img src="./quicc_for_dev.png" alt="Schema" align="middle">
+</div>
+
+>- Pour créer des champs (colonnes) de type raster, nous avons besoins de **l'extension PostGIS**...
+
+--- .segue .nobackground .dark
+
+## Qu'est-ce que PostGIS?
+
+--- &twocol
+
+## Qu'est ce que l'extension PostGIS ? 
+
+*** =left
+
+C'est une cartouche spatiale qui permet de stocker et de manipuler des objets spatiaux à l'intérieur de PostgreSQL.
+
+**Deux types d'objets spatiaux:**
+
+1. **Geometrique**
+    + Polygones (Multi ou simple)
+    + Lignes
+    + Points (Multi ou simple)
+2. **Raster/Grilles**
+
+*** =right
+
+<div style='text-align: center; margin-top:50px '>
+    <img src="./PostGIS_logo.png" alt="LogoPostgreSQL" align="middle">
+</div>
+
+
+--- &twocol
+
+## Qu'elles sont les forces de PostGIS Raster? 
+
+*** =left
+
+**Avantages:**
+
+>1. Indexation spatiale
+>2. Stockage des grilles en héxadecimale (Diminue le poids des grilles)
+>3. Posséder des fonctions permettant de traiter spatialement ces objets nativement à l'intérieur de la base de données (p.ex. Clip, Insersection/Interception, Aggregation spatiale et temporelle)
+>4. Puissance de calcul (avec opérations matricielles)
+
+*** =right
+
+<div style='text-align: center; margin-top:50px'>
+    <img src="./PostGIS_logo.png" height="300" alt="LogoPostgreSQL" align="middle">
+</div>
+
+--- .segue .nobackground .dark
+
+## Volet 2: Importer les grilles climatiques...
+
+--- 
+
+## Des fichiers Matlab (HDF5) vers la base de données PostgreSQL...
+
+L'importation se fait dans l'infrastructure de Colosse (Super-Calculateur de l'U-Laval).
+
+<div style='text-align: center'>
+    <img src="./workflow_ouranos.png" alt="WorkFlow" align="middle">
+</div>
+
+Le programme raster2pgsql découpe chaque raster climatique en tuile de 38 x 20 pixels.
+
+Les grilles ne sont pas stockés en multibandes.
+
+--- 
+
+## Des fichiers Matlab (HDF5) vers la base de données PostgreSQL...
+
+Le programme et l'infrastructure permet de traiter **jusqu'à 24 fichiers Matlab à la fois**.
+
+**Vitesse de traitement d'un fichier HDF par le programme R/raster2pgsql**:
+
+- 12 minutes pour une importation des grilles mensuelles
+- 30 minutes pour une importation des grilles annuelles
+
+**Durée totale de l'importation:**
+
+- 10-12 heures pour les grilles mensuelles 
+- 24 heures pour les grilles annnuelles
+
+--- .segue .nobackground .dark
+
+## Sinon, ca ressemble à quoi dans la BD?
+
+--- 
+
+## Quelques chiffres après importation...
+
+**Poids des indexes et tables:**
+
+|                         Relation                         |         Poids (data)         | 
+|----------------------------------------------------------|------------------------------|
+| clim_rs.fut_clim_vars                                    | 358 GB (58 GB data)          |
+| clim_rs.fut_clim_biovars                                 | 187 GB (30 GB data)          |
+| clim_rs.idx_spatial_mth                                  | 6226 MB (index)              |
+| clim_rs.idx_mod_run_mth                                  | 4983 MB (index)              |
+| clim_rs.rs_pkey                                          | 4696 MB (index)              |
+| clim_rs.idx_spatial_bio                                  | 3277 MB (index)              |
+| clim_rs.idx_mod_run_bio                                  | 2622 MB (index)              |
+-------------------------------------------------------------------------------------------
+
+Table annuel avec 54 821 232 de lignes et mensuel avec 104 175 072 lignes
+
+
+--- .segue .nobackground .dark
+
+## Comment interroger cette BD?
+
+--- 
+
+## Le language SQL
+
+On interroge une base de données en utilisant le language SQL (Stuctured Query Language). 
+Il permet la manipulation des données via des instructions (`SELECT`, `INSERT`, `UPDATE`, `DELETE`...).
+
+
+<div style='text-align: center; margin-top:50px'>
+    <img src="./ex1.png" alt="LogoPostgreSQL" align="middle">
+</div>
+
+
+**Une requête simple**:
+
+    SELECT rast, year_clim FROM clim_rs.past_clim_allbiovars 
+    WHERE year_clim >= 1960 OR year_clim <=1970 AND biovar = 'annual_mean_temp';
+
+
+--- 
+
+## Exemple de requête de manipulation spatiale sur raster
+
+**Aggregation temporelle:**
+
+    SELECT ST_Union(rast,'MEAN') as tavg_6070, biovar FROM clim_rs.past_clim_allbiovars 
+    WHERE year_clim >= 1960 OR year_clim <= 1970
+    GROUP BY biovar;
+
+Liste des fonctions sur raster disponible sur [postgis.net](http://postgis.net/docs/manual-2.1/RT_reference.html) 
+
+--- .segue .nobackground .dark
+
+## Comment accéder à cette BD ?
+
+
+--- 
+
+## Via pgAdmin3...
+
+<div style='text-align: center'>
+    <img src="./pgAdmin3.png" alt="pgAdmin" align="middle">
+</div>
+
+--- 
+
+## Via une librairie (driver) dans son language préféré
+
+
+```r
+require(RPostgreSQL)
+
+dbname <- "quicc_for_dev"
+dbhost <- "localhost"
+dbuser <- 'postgres'
+dbport <- 5433
+drv <- dbDriver("PostgreSQL")
+
+# Connection à la base de données
+con <- dbConnect(drv, host=dbhost, port=dbport, dbname=dbname,user=dbuser)
+
+# Écriture de la requête
+query_tmax_60 <- "
+    SELECT * FROM clim_rs.past_clim_allbiovars 
+    WHERE biovar = 'annual_maximum_temp' AND year_clim = 1960"
+
+# Éxécution de la requête 
+res <- dbGetQuery(con,query_tmax_60)
+```
+
+--- 
+
+## Via une librairie (driver) dans son language préféré
+
+
+```r
+str(res)
+```
+
+```
+## 'data.frame':	696 obs. of  5 variables:
+##  $ rs_id    : int  2120847 2120870 2120878 2121336 2121307 2121308 2121309 2121310 2121311 2121312 ...
+##  $ filename : chr  "1960_sg300_14.asc" "1960_sg300_14.asc" "1960_sg300_14.asc" "1960_sg300_14.asc" ...
+##  $ year_clim: int  1960 1960 1960 1960 1960 1960 1960 1960 1960 1960 ...
+##  $ biovar   : chr  "annual_maximum_temp" "annual_maximum_temp" "annual_maximum_temp" "annual_maximum_temp" ...
+##  $ rast     : chr  "01000001001975ADBD4F55B53F1975ADBD4F55B5BF33A5F5B7040058C09DA223B9FCBF524000000000000000000000000000000000AD10000030001E004A003"| __truncated__ "01000001001975ADBD4F55B53F1975ADBD4F55B5BF226E4E2503005EC0AF0D15E3FC1F524000000000000000000000000000000000AD10000030001E004A003"| __truncated__ "01000001001975ADBD4F55B53F1975ADBD4F55B5BF39622D3E050056C0AF0D15E3FC1F524000000000000000000000000000000000AD10000030001E004A003"| __truncated__ "01000001001975ADBD4F55B53F1975ADBD4F55B5BF282B86AB03005CC0967D5704FF3F404000000000000000000000000000000000AD10000030001E004A003"| __truncated__ ...
+```
+
+--- 
+
+## Visualiser dans R
+
+<div style='text-align: center'>
+    <img src="./var1.jpg" alt="pgAdmin" align="middle">
+</div>
+
+--- .segue .nobackground .dark
+
+## Les Geoserveurs...
+
+--- 
+
+## Qu'est ce qu'un géoserveur ?
+
+> C'est un service (généralement) web fourni par un serveur et permettant à un client d'extraire des données spatialisées via les protocoles HTTP.
+
+**Il en éxiste trois sortes:**
+
+1. **Web Mapping Service (WMS):** Pour visualiser des vecteurs ou des grilles comme des images cartographiques (JPEG , PNG , TIFF)
+2. **Web Feature Service (WFS):** Pour extraire des données géographiques dans diverses format (GeoJSON, KML, GML)
+3. **Web Feature Service Transactional (WFS-T):** Permet d'interagir directement avec les base de données (édition des objets spatiaux envoyé par le serveur).
+
+--- 
+
+## La solution possible pour Ouranos
+
+Selon le livre, *Postgis in Action (Obe et Hsu, 2011)*, ce qui est recommandé pour vos besoins (soit 1. Visualisation et 2. Extraction) est [MapServer](http://mapserver.org/), car il supporte toutes les extensions de PostGIS.
+
+<div style='text-align: center; margin-top:50px'>
+    <img src="./mapServer.png" alt="LogoPostgreSQL" align="middle">
+</div>
+
+--- 
+
+## Un Exemple de Web Mapping Service (WMS) via R/Leaflet
+
+
+```r
+require(rCharts)
+require(knitr)
+map1 = Leaflet$new()
+map1$setView(c(52.731631, -70.156347), 4)
+map1$tileLayer("http://{s}.tile.openstreetmap.se/hydda/base/{z}/{x}/{y}.png")
+map1$set(width = 800, height = 550)
+map1$save('maps/map1.html', cdn = TRUE)
+```
+
+--- 
+
+<iframe data-src='maps/map1.html' seamless onload=lzld(this)></iframe>
+
+--- 
+
+## Connecter le Web Mapping Service (WMS) avec PostgreSQL
+
+Un exemple avec le Bas-Saint-Laurent...
+
+
+
+
+```r
+query_climBSL <- "SELECT (ST_Intersection(rast,bsl.geom)).*, rs_id
+
+FROM clim_rs.past_clim_allbiovars,
+(SELECT geom FROM map_qc.regio_s WHERE res_nm_reg = 'Bas-Saint-Laurent') as bsl
+
+WHERE year_clim=2010 AND biovar = 'annual_mean_temp'
+AND ST_Intersects(rast,bsl.geom);"
+```
+
+--- 
+
+<iframe data-src='maps/map2.html' seamless onload=lzld(this)></iframe>
+
+
+
+
+--- 
+
+### **Grille climatique de la température moyenne annuelle (°C) pour l'année 2010** 
+(Bas-Saint-Laurent, interpolation par ANUSPLIN)
+
+<iframe data-src='maps/map3.html' seamless onload=lzld(this)></iframe>
+
